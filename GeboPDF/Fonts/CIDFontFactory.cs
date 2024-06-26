3788a5fb4dadf92e61b604b9d75fbcda1b034a6f
gebodal <26224700+gebodal@users.noreply.github.com>
2024-06-26 19:16:27 +0100

    Move font metric extraction to TrueTypeFontMetrics.
    The algorithm to extract font metrics from TrueTypeFontFile objects has been moved to a separate class for better reusability. Also found a corrected a minor bug where the yMin value was not being extracted correctly.

--- GeboPDF/Fonts/CIDFontFactory.cs
+++ GeboPDF/Fonts/CIDFontFactory.cs
@@ -66,7 +66,7 @@ namespace GeboPdf.Fonts {
 			GlyphSubstitutionLookupSet? gsubLookups = fontFile.gsub?.GetLookups(layoutTags);
 			GlyphPositioningLookupSet? gposLookups = fontFile.gpos?.GetLookups(layoutTags);
 
-			(ushort[] advanceWidths, short[] ascents, short[] descents, Dictionary<uint, short>? kerning) = GetMetrics(fontFile);
+			(ushort[] advanceWidths, short[] ascents, short[] descents, Dictionary<uint, short>? kerning) = fontFile.GetMetrics();
 
 			PdfType0Font pdfFont = new PdfType0Font(
 				fontUri, memoryStream,
@@ -317,64 +317,6 @@ namespace GeboPdf.Fonts {
 			}
 
 			return array;
-		}
-
-		private static (ushort[] advanceWidths, short[] ascents, short[] descents, Dictionary<uint, short>? kerning) GetMetrics(TrueTypeFontFile fontFile) {
-
-			ushort[] advanceWidths = new ushort[fontFile.numGlyphs];
-			short[] ascents = new short[fontFile.numGlyphs];
-			short[] descents = new short[fontFile.numGlyphs];
-
-			for (int i = 0; i < fontFile.numGlyphs; i++) {
-				// Advance width
-				advanceWidths[i] = fontFile.hmtx.advanceWidths[i];
-
-				// Ascent & Descent
-				short yMax, yMin;
-				if (fontFile.glyf != null) {
-					yMax = fontFile.glyf.glyphs[i].yMax;
-					yMin = fontFile.glyf.glyphs[i].yMin;
-				}
-				else if (fontFile.os2 != null) {
-					yMax = fontFile.os2.sTypoAscender ?? fontFile.head.yMax;
-					yMin = fontFile.os2.sTypoDescender ?? fontFile.head.yMax;
-				}
-				else {
-					yMax = fontFile.head.yMax;
-					yMin = fontFile.head.yMax;
-				}
-				ascents[i] = yMax;
-				descents[i] = yMin;
-			}
-
-			Dictionary<uint, short>? kerning = null;
-			if (fontFile.kern is not null) {
-				kerning = new Dictionary<uint, short>();
-
-				foreach (uint pair in fontFile.kern.Subtables.SelectMany(s => s.Values.Keys).Distinct().OrderBy(p => p)) {
-
-					short kernValue = 0;
-
-					for (int s = 0; s < fontFile.kern.Subtables.Length; s++) {
-						TrueTypeKerningSubtable subtable = fontFile.kern.Subtables[s];
-						// TODO Only deals with horizontal writing
-						if(subtable.Coverage.IsKerningValues() && subtable.Coverage.IsHorizontal() && !subtable.Coverage.IsCrossStream()) {
-							if (subtable.Values.TryGetValue(pair, out short value)) {
-								if (subtable.Coverage.IsOverride()) {
-									kernValue = value;
-								}
-								else {
-									kernValue += value;
-								}
-							}
-						}
-					}
-
-					kerning[pair] = kernValue;
-				}
-			}
-
-			return (advanceWidths, ascents, descents, kerning);
 		}
 
 		public static IReadOnlySet<(ushort gid, ushort[] originals)> GetMappings(FontGlyphUsage fontUsage) {
---
+++ GeboPDF/Fonts/TrueType/TrueTypeFontMetrics.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeboPdf.Fonts.TrueType {
+
+	public static class TrueTypeFontMetrics {
+
+		public static (ushort[] advanceWidths, short[] ascents, short[] descents, Dictionary<uint, short>? kerning) GetMetrics(this TrueTypeFontFile fontFile) {
+
+			ushort[] advanceWidths = new ushort[fontFile.numGlyphs];
+			short[] ascents = new short[fontFile.numGlyphs];
+			short[] descents = new short[fontFile.numGlyphs];
+
+			for (int i = 0; i < fontFile.numGlyphs; i++) {
+				// Advance width
+				advanceWidths[i] = fontFile.hmtx.advanceWidths[i];
+
+				// Ascent & Descent
+				short yMax, yMin;
+				if (fontFile.glyf != null) {
+					yMax = fontFile.glyf.glyphs[i].yMax;
+					yMin = fontFile.glyf.glyphs[i].yMin;
+				}
+				else if (fontFile.os2 != null) {
+					yMax = fontFile.os2.sTypoAscender ?? fontFile.head.yMax;
+					yMin = fontFile.os2.sTypoDescender ?? fontFile.head.yMin;
+				}
+				else {
+					yMax = fontFile.head.yMax;
+					yMin = fontFile.head.yMin;
+				}
+				ascents[i] = yMax;
+				descents[i] = yMin;
+			}
+
+			Dictionary<uint, short>? kerning = null;
+			if (fontFile.kern is not null) {
+				kerning = new Dictionary<uint, short>();
+
+				foreach (uint pair in fontFile.kern.Subtables.SelectMany(s => s.Values.Keys).Distinct().OrderBy(p => p)) {
+
+					short kernValue = 0;
+
+					for (int s = 0; s < fontFile.kern.Subtables.Length; s++) {
+						TrueTypeKerningSubtable subtable = fontFile.kern.Subtables[s];
+						// TODO Only deals with horizontal writing
+						if (subtable.Coverage.IsKerningValues() && subtable.Coverage.IsHorizontal() && !subtable.Coverage.IsCrossStream()) {
+							if (subtable.Values.TryGetValue(pair, out short value)) {
+								if (subtable.Coverage.IsOverride()) {
+									kernValue = value;
+								}
+								else {
+									kernValue += value;
+								}
+							}
+						}
+					}
+
+					kerning[pair] = kernValue;
+				}
+			}
+
+			return (advanceWidths, ascents, descents, kerning);
+		}
+
+	}
+
+}
