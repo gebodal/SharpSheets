27df05aee4b2f06db41f177740da86b671b50a31
gebodal <26224700+gebodal@users.noreply.github.com>
2025-11-22 16:40:20 +0000

    Move Numbered/INumbered and ChildHolder to their own files.

---
+++ SharpSheets/Parsing/ChildHolder.cs
@@ -0,0 +1,12 @@
+﻿using SharpSheets.Widgets;
+
+namespace SharpSheets.Parsing {
+
+	public class ChildHolder {
+		public Div Child { get; }
+		public ChildHolder(Div child) {
+			this.Child = child;
+		}
+	}
+
+}
