3b0d34805a3409e5472dded8bad0779e5a0982a9
gebodal <26224700+gebodal@users.noreply.github.com>
2026-02-28 19:29:09 +0000

    Move Direction to a more appropriate place.
    This belongs in a more generic location, not in TitledBoxes.cs. Moved to Layouts, with its own file. No other changes required.

---
+++ SharpSheets/Layouts/Direction.cs
@@ -0,0 +1,25 @@
+﻿namespace SharpSheets.Layouts {
+
+	/// <summary>
+	/// Indicates a cardinal direction.
+	/// </summary>
+	public enum Direction {
+		/// <summary>
+		/// Indicates that the content should be positioned at the top, or oriented upwards.
+		/// </summary>
+		NORTH,
+		/// <summary>
+		/// Indicates that the content should be positioned on the right, or oriented rightwards.
+		/// </summary>
+		EAST,
+		/// <summary>
+		/// Indicates that the content should be positioned at the bottom, or oriented downwards.
+		/// </summary>
+		SOUTH,
+		/// <summary>
+		/// Indicates that the content should be positioned on the left, or oriented leftwards.
+		/// </summary>
+		WEST
+	}
+
+}
--- SharpSheets/Shapes/TitledBoxes.cs
+++ SharpSheets/Shapes/TitledBoxes.cs
@@ -6,28 +6,6 @@ using SharpSheets.Parsing;
 
 namespace SharpSheets.Shapes {
 
-	/// <summary>
-	/// Indicates a cardinal direction.
-	/// </summary>
-	public enum Direction {
-		/// <summary>
-		/// Indicates that the content should be positioned at the top, or oriented upwards.
-		/// </summary>
-		NORTH,
-		/// <summary>
-		/// Indicates that the content should be positioned on the right, or oriented rightwards.
-		/// </summary>
-		EAST,
-		/// <summary>
-		/// Indicates that the content should be positioned at the bottom, or oriented downwards.
-		/// </summary>
-		SOUTH,
-		/// <summary>
-		/// Indicates that the content should be positioned on the left, or oriented leftwards.
-		/// </summary>
-		WEST
-	}
-
 	public class BlockTitledBox : TitledBoxBase {
 
 		protected readonly TitlePosition position;
